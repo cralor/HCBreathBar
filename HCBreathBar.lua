bb3c709533c61e8dbfcb96e12455729bb7ca3b9d
cralor <cralor@users.noreply.github.com>
2023-04-29 12:30:38 -0500

    0.2
    fix scaling configuration

--- HCBreathBar.lua
+++ HCBreathBar.lua
@@ -5,7 +5,7 @@ local PLAY_SOUND_INTERVAL = 0.8 -- PLAY SOUND PER SECOND (number of seconds)
 local SOUND_FILE = 12889 -- SOUND PLAYED, REFERENCE https://www.wowhead.com/classic/sounds
 local SOUND_CHANNEL = "MASTER" -- SOUND CHANNEL USED FOR VOLUME ("MASTER", "SFX", "AMBIENCE", "MUSIC")
 local SOUND_THRESHOLD = 20 -- NUMBER OF SECONDS UNTIL SOUND PLAYS (seconds remaining)
-local BREATH_BAR_SCALE = 0.5 -- INCREASE IN SIZE OF BREATH BAR (0, 1)
+local BREATH_BAR_SCALE = 150 -- INCREASE IN SIZE OF BREATH BAR (%)
 
 -- [[ CONFIGURATION SETTINGS -- END ]]
 
@@ -15,7 +15,7 @@ for index = 1, MIRRORTIMER_NUMTIMERS, 1 do
     local lastPlayedSound = 0
     frame:HookScript("OnUpdate", function(self)
         if (self.timer == "BREATH") then
-            frame:SetScale(1.5)
+            frame:SetScale(BREATH_BAR_SCALE / 100)
             local Min = math.floor(self.value / 60)
             local Sec = math.floor(self.value - (Min * 60))
             text:SetText("Breath (" .. string.format("%01d:%02d",Min,Sec) .. ")")
